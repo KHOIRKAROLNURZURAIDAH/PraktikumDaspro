865fc45d695c5a47151bc45d9d8c411f0bb32bdf
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-09-20 12:00:28 +0700

    Percobaan1

---
+++ ContohVariabel16.java
@@ -0,0 +1,17 @@
+public class ContohVariabel16 {
+    public static void main(String[] args) {
+        String salahSatuHobiSayaAdalah = "Berkebun";
+boolean isPandai = true;
+char jenisKelamin = 'K';
+byte _umurSayaSekarang = 20;
+double $ipk = 3.50, tinggi = 1.57;
+
+System.out.println(salahSatuHobiSayaAdalah);
+System.out.println("Apakah pandai? " + isPandai);
+System.out.println("Jenis kelamin: " + jenisKelamin);
+System.out.println("Ipk:" + $ipk);
+System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+
+    }
+}
+
