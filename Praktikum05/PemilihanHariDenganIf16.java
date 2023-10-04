3c61829744cafa437e551597555470357f124e24
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-10-04 14:43:54 +0700

    pemilihan bilangan

---
+++ Praktikum05/PemilihanHariDenganIf16.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class PemilihanHariDenganIf16 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Praktikum05/PemilihanOperator16.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class PemilihanOperator16 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
