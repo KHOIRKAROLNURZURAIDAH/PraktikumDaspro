7c771c3755b78b3589fce40d0965cfe8bbd9c79e
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-10-11 14:52:27 +0700

    Segitigaa16

--- Segitigaa16.java
+++ Segitigaa16.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Segitigaa16 {
 
     public static void main(String[] args) {
+        
         Scanner input16 = new Scanner (System.in);
         float sudut1,sudut2, sudut3;
         double totalsudut;
@@ -16,13 +17,18 @@ public class Segitigaa16 {
         totalsudut = sudut1 + sudut2 + sudut3;
 
         if (totalsudut == 180){
-            if ((sudut1 == 90) || (sudut3 == 90))
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                  System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else 
-                 System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-        }   else
-                System.out.println("Bukan Segitiga");
+        }   else if ((sudut1 == sudut2 && sudut1 == sudut3  && sudut2 == sudut3)) {
+                 System.out.println("Segitiga tersebut adalah sama sisi");
+        }   else if ((sudut1 == sudut2 || sudut1 == sudut3 || sudut3 == sudut2)) {
+                 System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+        }   else {
+            System.out.println("Bukan Segitiga");
+        }
+                
     }
-    
 }
+    
+        
 
