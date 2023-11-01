d351d56af5b2485742bd3adb2481bca698c7a0bc
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-01 16:35:40 +0700

    LinierSearch

--- Praktikum09/LinierSearch16.java
+++ Praktikum09/LinierSearch16.java
@@ -2,10 +2,20 @@ package Praktikum09;
 import java.util.Scanner;
 public class LinierSearch16 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
         int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63} ;
-        int key = 20;
-        int hasil = 0;
+        int key, jumlahEleman;
+        int hasil = -1;
+
+        System.out.print("Masukan jumlah Elemen array: " );
+        jumlahEleman = sc.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukan Elemen array ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+         System.out.print("Masukan key yaang ingin dicari: ");
+         key = sc.nextInt();   
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -13,7 +23,12 @@ public class LinierSearch16 {
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        if (hasil != -1){
+            System.out.print("Key ada di posisi indeks ke- " + hasil);
+        }else{
+            System.out.print("Key tidak ditemukan");
+        }
+        
     }
     
 }
