446b9c71a6c592a18044af4356a2f6e0f171c6e8
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-01 11:56:58 +0700

    ArrayNilai16

---
+++ Praktikum09/ArrayNilai16.java
@@ -0,0 +1,17 @@
+package Praktikum09;
+import java.util.Scanner;
+public class ArrayNilai16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukan nilai akhir mahasiswa ke-" + (i+1) + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + "adalah " + nilaiAkhir[i] );
+        }
+    }
+    
+}
