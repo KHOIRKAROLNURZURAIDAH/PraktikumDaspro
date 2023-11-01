27798e270c4ae32faebc668fc037c5c801cf6e65
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-01 11:21:48 +0700

    ArrayBilangan16

--- Josheet07/TugasProjekPerpus07.java
+++ Josheet07/TugasProjekPerpus07.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+1import java.util.Scanner;
 public class TugasProjekPerpus07 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
