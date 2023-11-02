44a69bf8f049e61b903cc25f0c8cd8b31b731968
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-02 22:40:01 +0700

    LatihanIndividu.1

--- Praktikum09/LatihanIndividu16.java
+++
@@ -1,8 +0,0 @@
-package Praktikum09;
-import java.util.Scanner;
-public class LatihanIndividu16 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
---
+++ Praktikum09/LatihanIndividuno2.java
@@ -0,0 +1,18 @@
+package Praktikum09;
+import java.util.Scanner;
+public class LatihanIndividuno2 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i +1;
+        }
+        System.out.print("Isi array secara terbalik: ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
