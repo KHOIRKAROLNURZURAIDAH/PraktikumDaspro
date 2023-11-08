d748571a6bf99958e55cfe1616554aa1d1d7bd33
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-08 13:26:16 +0700

    Bioskop2

--- Praktikum10/Bioskop16.java
+++ Praktikum10/Bioskop16.java
@@ -15,8 +15,9 @@ public class Bioskop16 {
 
      
     System.out.println(penonton.length);
-    for (int i = 0; i < penonton.length; i++) {
-        System.out.println("Panjang baris ke-" + (i+1) +": " + penonton[i].length);
+
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris: " + barisPenonton.length);
     }
         
     System.out.println(penonton[0].length);
