5c3efae42d64c6011e8b0c946d393a0d99a8da13
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-11-29 11:26:06 +0700

    Welcome16

---
+++ Praktikum13/Welcome16.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Welcome16 {
+    public static void displayWelcomeMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+}
