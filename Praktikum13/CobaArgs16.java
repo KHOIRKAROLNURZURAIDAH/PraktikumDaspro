1d4fcc61ad3180564a972aa6daad1b84e03c7b6c
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-12-06 11:31:02 +0700

    CobaArgs

--- Praktikum13/CobaArgs16.java
+++ Praktikum13/CobaArgs16.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 public class CobaArgs16 {
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+  if (args.length == 0) {
+       System.out.println("Hai, selamat datang");
+     }else if (args.length == 2) {
+       String nama = args[0];
+       String tempat = args[1];
+       System.out.println(("Hai " + nama));
+       System.out.println("Selamat datang di " +tempat);
+     }
     }
   }
+ 
+ 
+ 
+      //      System.out.println("Hello world!");
+
+  //      for (int i = 0; i < args.length; i++) {
+  //       System.out.println(args[i]);
+  //      }
+  //   }
+  // }
       
 
   
@@ -19,17 +36,7 @@ public class CobaArgs16 {
   
   
   
-      //  if (args.length == 0) {
-    //    System.out.println("Hai, selamat datang");
-    //  }else if (args.length == 2) {
-    //    String nama = args[0];
-    //    String tempat = args[1];
-     //   System.out.println(("Hai " + nama));
-      //  System.out.println("Selamat datang di " +tempat);
-    //  }
-  //}
-//}
-
+       
     
     
     
